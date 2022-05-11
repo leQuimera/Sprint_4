5ad3b40ec818a97a5b85f87f3508f7931b58a82d
lequimera@yandex.ru <Slonik600>
2022-05-11 21:55:08 +0300

    Commit class

---
+++ src/main/java/ru/yandex/Praktikum.java
@@ -0,0 +1,21 @@
+package ru.yandex;
+
+import java.security.PrivateKey;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+
+        String accountName = "Тимоте Шаламе";
+
+        /*
+             В этом методе заложи логику работы с классом Account.
+             Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
+             и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
+         */
+
+        Account account = new Account(accountName);
+        System.out.println(account.checkNameToEmboss());
+    }
+
+}
